3454d339750534eac52feee924b2b192f973a412
krolos Name <krolosshaker25@gmail.com>
2023-03-10 09:48:20 -0800

    create_static_lib.sh

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,10 @@
-include "main.h"
+#include "main.h"
 
-int main(void)
+int main(void);
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+_puts("\"At the end of the day,");
+}
+{
+_puts(" my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
