104f3664344ef68f580b4c10d7729572ba4a4ceb
krolos Name <krolosshaker25@gmail.com>
2023-03-12 23:39:37 -0700

    0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - uppercase letters
  * @c: char to check
@@ -8,8 +7,8 @@
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
