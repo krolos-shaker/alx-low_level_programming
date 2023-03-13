212362192969f5d4fe6b1a890dc144bacaf942dd
krolos Name <krolosshaker25@gmail.com>
2023-03-13 00:13:10 -0700

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: Always 0 (Success)
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
+while (*str != '/0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
 }
