933cb7e8c94289476bd4a8be29c0da32eddddd6e
krolos-shaker <krolosshaker25@gmail.com.com>
2023-04-08 09:44:00 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,36 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - read a text file and print it to stdout
- * @filename: the name of the file to read
- * @letters: the number of letters to be read and printed
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: If filename is NULL, the file cannot be opened or read, or
- * write fails or returns an unexpected number of bytes, return 0.
- * Otherwise, return the actual number of letters read and printed.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = NULL;
-	ssize_t b_read;
-	ssize_t b_written;
 	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (!(filename && letters))
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	b_read = read(fd, buffer, letters);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
 
-	if (b_read < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	if (!b_read)
-		b_read = letters;
+	free(buf);
 
-	b_written = write(STDOUT_FILENO, buffer, b_read);
-	free(buffer);
-
-	if (b_written < 0)
-		return (0);
-
-	return (b_written);
+	return (nwr);
 }
