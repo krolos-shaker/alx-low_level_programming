bfd879a38ec92c772dcfa791c529e39f5d34776c
krolos-shaker <krolosshaker25@gmail.com.com>
2023-04-08 10:07:47 -0700

    1-create_file.c

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: %s filename text\n", av[0]);
 		exit(1);
 	}
-       if(av[1], av[2]);
-       printf("-> %i)\n", res);
-       return (0);
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
